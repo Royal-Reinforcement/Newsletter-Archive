3f01a77a9139de2125cee34c55338a21dc6d66fd
Ralph McCracken <ralph@royaldestinations.com>
2025-06-05 09:39:27 -0500

    authentication

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import smartsheet
-
 import streamlit.components.v1 as components
 
 
@@ -16,24 +15,33 @@ def smartsheet_to_dataframe(sheet_id):
 
 st.set_page_config(page_title='Newsletter Archive | RD', page_icon='📬', layout='wide')
 
-st.image(st.secrets["logo"], width=100)
+st.image(st.secrets['logo'], width=100)
 
 st.header('Newsletter Archive')
-st.info('Belew are our prior outreaches for your convenience.')
 
-df         = smartsheet_to_dataframe(st.secrets['smartsheet']['sheet_id']['record'])
-df         = df.dropna()
-categories = df['Category'].unique().tolist()
-tabs       = st.tabs(categories)
+auth = smartsheet_to_dataframe(st.secrets['smartsheet']['sheet_id']['authentication'])
+auth = auth.dropna()
+auth = auth.Key.values[0]
 
-for category, tab in zip(categories, tabs):
-    with tab:
-        category_df          = df[df.Category == category].copy()
-        category_df['Dated'] = pd.to_datetime(category_df['Dated'])
-        category_df          = category_df.sort_values(by='Dated', ascending=False)
+if st.query_params.auth != auth:
+    st.warning('It appears you are using an expired archive link. Please refer to the **most recent newsletter** for the current link.')
 
-        def produce_iframes(row):
-            with st.expander(label=f"{row['Title']}"):
-                components.iframe(src=row['URL'], height=375, scrolling=True)
-        
-        category_df.apply(produce_iframes, axis=1)
+else:
+    st.info('Belew are our prior outreaches for your convenience.')
+
+    df         = smartsheet_to_dataframe(st.secrets['smartsheet']['sheet_id']['record'])
+    df         = df.dropna()
+    categories = df['Category'].unique().tolist()
+    tabs       = st.tabs(categories)
+
+    for category, tab in zip(categories, tabs):
+        with tab:
+            category_df          = df[df.Category == category].copy()
+            category_df['Dated'] = pd.to_datetime(category_df['Dated'])
+            category_df          = category_df.sort_values(by='Dated', ascending=False)
+
+            def produce_iframes(row):
+                with st.expander(label=f"{row['Title']}"):
+                    components.iframe(src=row['URL'], height=375, scrolling=True)
+            
+            category_df.apply(produce_iframes, axis=1)
